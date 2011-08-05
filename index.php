d2fbad56b86afa311420d6754cebe3051b675f59
lohnn <lohnn@lohnn-W7>
2011-08-05 02:45:29 +0200

    Added index.

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
