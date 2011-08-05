0b4e2cf916c62a583480414322df4a61446ded2a
lohnn <lohnn@lohnn-W7>
2011-08-05 04:16:44 +0200

    Some minor changes.

--- example1.js
+++ example1.js
@@ -1,28 +1,28 @@
 var labelType, useGradients, nativeTextSupport, animate;
 
 (function() {
-  var ua = navigator.userAgent,
-      iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
-      typeOfCanvas = typeof HTMLCanvasElement,
-      nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
-      textSupport = nativeCanvasSupport 
-        && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
-  //I'm setting this based on the fact that ExCanvas provides text support for IE
-  //and that as of today iPhone/iPad current text support is lame
-  labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML';
-  nativeTextSupport = labelType == 'Native';
-  useGradients = nativeCanvasSupport;
-  animate = !(iStuff || !nativeCanvasSupport);
+    var ua = navigator.userAgent,
+    iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
+    typeOfCanvas = typeof HTMLCanvasElement,
+    nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
+    textSupport = nativeCanvasSupport 
+    && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
+    //I'm setting this based on the fact that ExCanvas provides text support for IE
+    //and that as of today iPhone/iPad current text support is lame
+    labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML';
+    nativeTextSupport = labelType == 'Native';
+    useGradients = nativeCanvasSupport;
+    animate = !(iStuff || !nativeCanvasSupport);
 })();
 
 var Log = {
-  elem: false,
-  write: function(text){
-    if (!this.elem) 
-      this.elem = document.getElementById('log');
-    this.elem.innerHTML = text;
-    this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px';
-  }
+    elem: false,
+    write: function(text){
+        if (!this.elem) 
+            this.elem = document.getElementById('log');
+        this.elem.innerHTML = text;
+        this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px';
+    }
 };
 
 
@@ -395,16 +395,16 @@ function init(){
         //Optional: create a background canvas that plots
         //concentric circles.
         background: {
-          CanvasStyles: {
-            strokeStyle: '#555'
-          }
+            CanvasStyles: {
+                strokeStyle: '#555'
+            }
         },
         //Add navigation capabilities:
         //zooming by scrolling and panning.
         Navigation: {
-          enable: true,
-          panning: true,
-          zooming: 20
+            enable: true,
+            panning: true,
+            zooming: 20
         },
         //Set Node and Edge styles.
         Node: {
@@ -412,15 +412,15 @@ function init(){
         },
         
         Edge: {
-          color: '#C17878',
-          lineWidth:1.5
+            color: '#FF6600',
+            lineWidth:2
         },
 
         onBeforeCompute: function(node){
-            Log.write("centering " + node.name + "...");
-            //Add the relation list in the right column.
-            //This list is taken from the data property of each JSON node.
-            $jit.id('inner-details').innerHTML = node.data.relation;
+        //Log.write("centering " + node.name + "...");
+        //Add the relation list in the right column.
+        //This list is taken from the data property of each JSON node.
+        //$jit.id('inner-details').innerHTML = node.data.relation;
         },
         
         //Add the name of the node in the correponding label
@@ -431,7 +431,7 @@ function init(){
             domElement.onclick = function(){
                 rgraph.onClick(node.id, {
                     onComplete: function() {
-                        Log.write("done");
+                    //Log.write("done");
                     }
                 });
             };
@@ -449,7 +449,7 @@ function init(){
             
             } else if(node._depth == 2){
                 style.fontSize = "0.7em";
-                style.color = "#494949";
+                style.color = "#636363";
             
             } else {
                 style.display = 'none';
@@ -464,15 +464,18 @@ function init(){
     rgraph.loadJSON(json);
     //trigger small animation
     rgraph.graph.eachNode(function(n) {
-      var pos = n.getPos();
-      pos.setc(-200, -200);
+        var pos = n.getPos();
+        pos.setc(-200, -200);
     });
     rgraph.compute('end');
     rgraph.fx.animate({
-      modes:['polar'],
-      duration: 1250
+        modes:['polar'],
+        duration: 1250
     });
-    //end
-    //append information about the root relations in the right column
-    $jit.id('inner-details').innerHTML = rgraph.graph.getNode(rgraph.root).data.relation;
+    window.onresize = function(){
+        rgraph.canvas.resize(window.innerWidth, window.innerHeight);
+    }
+//end
+//append information about the root relations in the right column
+//$jit.id('inner-details').innerHTML = rgraph.graph.getNode(rgraph.root).data.relation;
 }
